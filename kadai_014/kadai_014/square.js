4c87119f5358b4c4c6897b9d45eaf9ab9050d762
yuri29-saint-Ex <140218565+yuri29-saint-Ex@users.noreply.github.com>
2023-10-17 13:50:17 +0900

    Delete kadai_014 directory

--- kadai_014/kadai_014/square.js
+++
@@ -1,8 +0,0 @@
-
-  //与えらえた引数numを二乗し、その値を戻り値として返す関数を定義する
-const squared  = (num) => { 
-   return num * num;
-}
-
-//関数の戻り値を出力する
-console.log(squared(10));
--- kadai_014/square.js
+++
@@ -1,7 +0,0 @@
-const num  = (squared) => {
-  //与えらえた引数numを二乗し、その値を出力する
-  console.log(squared * squared);
-}
-
-//関数を呼び出し、引数としてsquaredの値を渡す
-num(10);
